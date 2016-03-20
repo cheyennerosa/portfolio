4656bfeb6a40ddb625866b420d31f51ca7aef445
Cheyenne Thayer <cthayer09@gmail.com>
2016-03-19 22:03:27 -0700

    made lots of changes today. page is fully responsive. menu is complete. working on the content now. was considering using PHP for templates only but it's not necessary without a DB and requires a VE running. which is not that complicated but python is being bad right now. going to focus on home page content.

---
+++ js/load_essentials.js
@@ -0,0 +1,16 @@
+// document.getElementById("myHead").innerHTML =
+// 	"<span id='headerText'>Title</span>"
+// 	+ "<span id='headerSubtext'>Subtitle</span>";
+
+// document.getElementById("myNav").innerHTML =
+// 	"<ul id='navLinks'>"
+// 	+ "<li><a href='index.html'>Home</a></li>"
+// 	+ "<li><a href='about.html'>About</a>"
+// 	+ "<li><a href='donate.html'>Donate</a></li>"
+// 	+ "</ul>";
+
+document.getElementById("footer").innerHTML =
+	"<p id='copyright'>Copyright &copy; " + new Date().getFullYear() + "</p>"
+	+ "<p>All rights reserved.</p>"
+	+ "<p id='credits'>Design by Cheyenne R Thayer</p>"
+	+ "<p id='contact'><a href='mailto:cthayer09@gmail.com'>Contact Me</a>"
