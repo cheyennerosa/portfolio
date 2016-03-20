ce5ac619c24e19f286d9878388ee59636bb7ca11
Cheyenne Thayer <cthayer09@gmail.com>
2016-03-20 14:06:43 -0700

    attempting to add a class to body when menu is open

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
 var body = document.body,
-	nav = document.getElementsByTagName("nav"),
-	button = document.getElementsByTagName("button"),
-	menu = document.getElementsByClassName('overlay'),
-	open = document.getElementsByClassName('btn-close');
+	button = document.getElementById("trigger-overlay");
 
 if (classie.has( button, 'btn-close')) {
     classie.add( body, 'disable-scroll');
+    console.log("wut hapening?")
 } else {
     classie.remove( body, 'disable-scroll');
 }
