c89ce285ff3ac17bd3f8afa744c3739bfa8bfe40
Cheyenne Thayer <cthayer09@gmail.com>
2016-05-21 19:06:45 -0700

    still not sure why css is not being applied to the body...

--- js/_header.js
+++ js/_header.js
@@ -10,4 +10,4 @@ document.getElementById("funky-header").innerHTML =
 	+ "<li><a href='work.html'>Work</a></li>"
 	+ "<li><a href='contact.html'>Contact</a></li>"
 	+ "</ul>"
-	+ "</nav>"
+	+ "</nav>";
--- js/head.js
+++ js/head.js
@@ -11,17 +11,3 @@ document.getElementById("head").innerHTML =
 	+ "<link href='css/animate.css' type='text/css' rel='stylesheet' media='screen'>";
 
 document.title = 'Cheyenne Rosa Portfolio'; // New title :)
-
-
-// document.getElementById("myNav").innerHTML =
-// 	"<ul id='navLinks'>"
-// 	+ "<li><a href='index.html'>Home</a></li>"
-// 	+ "<li><a href='about.html'>About</a>"
-// 	+ "<li><a href='donate.html'>Donate</a></li>"
-// 	+ "</ul>";
-
-document.getElementById("cool-footer").innerHTML =
-	"<p id='copyright'>Copyright &copy; " + new Date().getFullYear() + "</p>"
-	+ "<p>All rights reserved.</p>"
-	+ "<p id='credits'>Design by Cheyenne R Thayer</p>"
-	+ "<p id='contact'><a href='mailto:cthayer09@gmail.com'>Contact Me</a>";
